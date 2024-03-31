1a5f8aefab528ed585a9a7da1695984970cda99b
michallow <michallo_w@o2.pl>
2024-03-31 21:43:53 +0200

    Upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Profile } from './profile/Profile';
+import user from './profile/user.json';
+// import { Statistics } from './statistics/Statistics';
+// import data from './statistics/data.json';
+// import { FriendsList } from './friendsList/FriendsList';
+// import friends from './friendsList/friends.json';
+// import { TransactionHistory } from './transactionHistory/TransactionHistory';
+// import transactions from './transactionHistory/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      ></Profile>
+      {/* <Statistics title={data.title} stats={data}></Statistics>
+      <FriendsList friends={friends}></FriendsList>
+      <TransactionHistory transactions={transactions}></TransactionHistory> */}
     </div>
   );
 };
