c5cff9bbd2b984ee7f810f04accc0467b7544140
michallow <michallo_w@o2.pl>
2024-04-02 08:28:01 +0200

    Styles update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,7 +9,7 @@ export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
-      <ul className={css['stat-list']}>
+      <ul className={css.statList}>
         {stats.map(stat => (
           <li
             className={css.item}
